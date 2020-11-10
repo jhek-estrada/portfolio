e4e32e7817de4d95ed6ef2814966de4254d82df2
JHERIKO ESTRADA <jheriko.estrada@ibm.com>
2020-11-10 10:42:32 +0800

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,148 @@
+let resumeData = {
+  "imagebaseurl":"",
+  "name": "Jheriko Estrada",
+  "role": "Fullstack Developer",
+  "linkedinId":"https://github.com/jhek-estrada",
+  "skypeid": "Your skypeid",
+  "roleDescription": "Dedicated and efficient full stack developer with 4+ years experience in web development, windows application development, and databases.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/jheriko-estrada/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/jhek-estrada",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"https://join.skype.com/invite/kttmCqOo04yz",
+        "className":"fa fa-skype"
+      },
+      {
+        "name":"facebook",
+        "url":"https://facebook.com/ryko15",
+        "className":"fa fa-facebook"
+      }
+    ],
+  "aboutme":"I have finished my B.S. in Information Technology and have been working in industry for past few years.",
+  "address":"Philippines",
+  "website":"",
+  "education":[
+    {
+      "UniversityName":"ABE International Business College",
+      "specialization":"Web Development, Database optimization, Desktop development",
+      "MonthOfPassing":"Apr",
+      "YearOfPassing":"2016",
+      "Achievements":""
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"International Business Machines Corporation (IBM)",
+      "specialization":"Oracle PL/SQL, Desktop Development, Web Development",
+      "MonthStarted":"Nov",
+      "YearStarted":"2019",
+      "MonthOfLeaving":"Oct",
+      "YearOfLeaving":"2020",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Aptus Global Solutions Inc.",
+      "specialization":"Web development using Laravel and WordPress",
+      "MonthStarted":"Apr",
+      "YearStarted":"2019",
+      "MonthOfLeaving":"Oct",
+      "YearOfLeaving":"2019",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Marketing Convergence Inc.",
+      "specialization":"Oracle PL/SQL, Desktop Development, Web Development",
+      "MonthStarted":"Apr",
+      "YearStarted":"2018",
+      "MonthOfLeaving":"Apr",
+      "YearOfLeaving":"2019",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Business Machines Corporation",
+      "specialization":"Oracle PL/SQL, Desktop Development, Web Development",
+      "MonthStarted":"Aug",
+      "YearStarted":"2016",
+      "MonthOfLeaving":"Apr",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Skills used for Developing",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"JavaScript"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"WordPress"
+    },
+    {
+      "skillname":"PHP"
+    },
+    {
+      "skillname":"MySQL"
+    },
+    {
+      "skillname":"MSSQL"
+    },
+    {
+      "skillname":"PLSQL"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"YHotels",
+      "description":"WordPress Application",
+      "imgurl":"images/portfolio/yhotel.png",
+      "url":"https://yhotels.ph/"
+    },
+    {
+      "name":"Atlantis Hotel",
+      "description":"WordPress Application",
+      "imgurl":"images/portfolio/atlantis.png",
+      "url":"https://atlantishotel.com/"
+    },
+    {
+      "name":"Star8 Shop",
+      "description":"WordPress Application",
+      "imgurl":"images/portfolio/star8.png",
+      "url":"https://star8shop.com/"
+    },
+    {
+      "name":"Silya Mesa",
+      "description":"WordPress Application",
+      "imgurl":"images/portfolio/silya.png",
+      "url":"https://silya-mesa.com/"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"Jheriko has helped us the best with redesigning our website. He reduced its loading time by implementing the right technology but also ensured that it is attractive and easy-to-use, ensured seamless communication for managing exigencies and delivered regular updates. Highly recommend them if you are looking for great output.",
+      "name":"Former Client"
+    },
+    {
+      "description":"The work was done on time, on budget, and with a high degree of professionalism.",
+      "name":"Former Client"
+    }
+  ]
+}
+
+export default resumeData
